b185ecaa8026993b342f0317b6555b60d0ab34fb
Temistocle <giacomo.mazzini@ipratico.it>
2025-10-11 00:36:33 +0200

    Set --experimental_allow_proto3_optional flag to pass Cargo's release build of grpc_server

--- src/grpc_server/build.rs
+++ src/grpc_server/build.rs
@@ -1,3 +1,13 @@
+/*
 fn main() {
     tonic_build::compile_protos("proto/prediction/prediction.proto").unwrap();
 }
+*/
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tonic_build::configure()
+        .protoc_arg("--experimental_allow_proto3_optional")
+        .compile_protos(&["proto/prediction/prediction.proto"], &["proto"])?;
+    Ok(())
+}
+
